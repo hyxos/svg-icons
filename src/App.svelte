d783b2421e2a6409de196139b55ebdecbb099a99
Donnie Hicks <dw@1hx.ca>
2019-09-19 22:48:22 -0400

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,45 @@
+<script>
+  export let name;
+  import Icon from './Icon.svelte'
+  let icons = [
+    "bathroom",
+    "beach",
+    "bed",
+    "car",
+    "chef",
+    "child-care",
+    "fishing",
+    "horseback-riding",
+    "location",
+    "people",
+    "pool",
+    "sailing",
+    "sun",
+    "surf-board",
+    "volcano",
+    "water",
+    "yoga"
+  ]
+</script>
+
+<style>
+  h1 {
+    font-size: 40px;
+  }
+	#icons {
+    display: flex;
+    justify-content: center;
+    flex-direction: row;
+    text-align: center;
+    align-content: flex-start;
+    flex-wrap: wrap;
+    max-width: 500px;
+  }
+</style>
+
+<h1>SVG Icons</h1>
+<div id="icons">
+  {#each icons as icon}
+    <Icon name={icon} />
+  {/each}
+</div>
---
+++ src/Icon.svelte
@@ -0,0 +1,7 @@
+<script>
+  export let name
+</script>
+<div class="icon">
+  <img name="{name}" id="{name}" alt="{name}" src="./svgs/{name}.svg" />
+  <label for="{name}">{name}</label>
+</div>
